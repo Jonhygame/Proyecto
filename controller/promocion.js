e941aff46fea49b4a9512240c76d270b15860d05
Jonathan Josafat Contreras Villanueva <contreras_villanueva@live.com.mx>
2022-11-18 21:07:48 -0600

    agrege el controlador promocion

---
+++ controller/promocion.js
@@ -0,0 +1,18 @@
+/** @format */
+
+function promosion(cual, para1, para2, para3) {
+  switch (cual) {
+    case value:
+      break;
+
+    default:
+      $.alert({
+        title: "Atencion!",
+        content:
+          '<span class="text-danger" La opcion <b>' +
+          cual +
+          "</b> No esta programada en promocion.js</span>",
+      });
+      break;
+  }
+}
